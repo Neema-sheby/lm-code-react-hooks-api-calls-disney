2d563e05eb57feed1d43e10d835c4fa13dccb367
neema <neemadas@gmail.com>
2023-02-05 23:19:35 +0000

    fix: Add fetch and useEffect to call the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Header from "./components/header";
+import CharacterContainer from "./components/character_container";
+import Navigation from "./components/navigation";
+import { DisneyCharacter } from "./disney_character";
 
-import './App.css';
-import React, { useState } from 'react';
-import Header from './components/header';
-import CharacterContainer from './components/character_container';
-import Navigation from './components/navigation';
-import { DisneyCharacter } from './disney_character';
+const App: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
 
-const App : React.FC = () => {
+  useEffect(() => {
+    const fetchDataHandler = async () => {
+      const response = await fetch("https://api.disneyapi.dev/characters");
+      const { data } = await response.json();
 
-	const [currentPage, setCurrentPage] = useState<number>(1);
+      console.log(data);
 
-  // Some dummy state representing disney characters
-  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
-    {
-      _id: 6,
-      name: "'Olu Mel",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
-    },
-    {
-      _id: 25,
-      name: "Abu",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Abu.png"
-    },
-    {
-      _id: 30,
-      name: "Ace",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
-    },
-  ]);
-
-  
+      const disneyData: Array<DisneyCharacter> = data.map(
+        (obj: DisneyCharacter) => {
+          return {
+            id: obj._id,
+            name: obj.name,
+            imageUrl: obj.imageUrl,
+          };
+        }
+      );
+      setCharacters(disneyData);
+    };
+    fetchDataHandler();
+  }, [currentPage]);
 
   return (
     <div className="page">
@@ -38,6 +37,6 @@ const App : React.FC = () => {
       <CharacterContainer characters={characters} />
     </div>
   );
-}
+};
 
 export default App;
